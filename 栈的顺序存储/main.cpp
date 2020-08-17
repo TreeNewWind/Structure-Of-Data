96152ab9df51d48ab75de70e2926d0c085a2fc38
TreeNewWind <31409729+TreeNewWind@users.noreply.github.com>
2020-08-17 11:49:16 +0800

    栈与括号实现

--- 栈的顺序存储/main.cpp
+++ 栈的顺序存储/main.cpp
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-#define MaxSize 10
+#define MaxSize 20
 
 
 
 typedef struct
 {
-    int data[MaxSize];
+    char data[MaxSize];
     int top; //Ö¸ÏòÕ»¶¥ÔªËØ
 }SqStack;
 
@@ -28,7 +28,7 @@ bool StackEmpty(SqStack S)
     }
 }
 
-bool Push(SqStack &S, int x)
+bool Push(SqStack &S, char x)
 {
     if(S.top == MaxSize - 1)
         return false; //Õ»Âú
@@ -37,7 +37,7 @@ bool Push(SqStack &S, int x)
     return true;
 }
 
-bool Pop(SqStack &S, int &x)
+bool Pop(SqStack &S, char &x)
 {
     if(S.top == -1)
         return false;
@@ -46,5 +46,50 @@ bool Pop(SqStack &S, int &x)
     return true;
 }
 
+bool brackerCheck(char str[], int length)
+{
+    int i;
+    SqStack S;
+    for(i = 0; i < length; i++)
+    {
+        if(str[i] == '(' || str[i] == '[' || str[i] == '{')
+            {
 
+                Push(S, str[i]);
+            }
+        else
+        {
+            if(StackEmpty(S))
+                return false;
+
+            char topElem;
+            Pop(S, topElem);
+
+            if(str[i] == ')' && topElem != '(')
+                return false;
+            if(str[i] == ']' && topElem != '[')
+                return false;
+            if(str[i] == '}' && topElem != '{')
+                return false;
+        }
+    }
+    return StackEmpty(S);
+}
+
+int main()
+{
+    char s[5];
+    scanf("%s", s);
+
+
+    bool flag;
+    flag = brackerCheck(s, 5);
+
+    if(flag)
+        printf("YES\n");
+    else
+        printf("NO\n");
+    return 0;
+
+}
 
