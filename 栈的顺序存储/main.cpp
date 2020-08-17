758e891bbbeba0ebf72483fa00fc97dff2ef5085
TreeNewWind <31409729+TreeNewWind@users.noreply.github.com>
2020-08-17 11:54:16 +0800

    栈的顺序存储

--- 栈的顺序存储/main.cpp
+++ 栈的顺序存储/main.cpp
@@ -6,7 +6,7 @@
 
 typedef struct
 {
-    char data[MaxSize];
+    ElemType data[MaxSize];
     int top; //Ö¸ÏòÕ»¶¥ÔªËØ
 }SqStack;
 
@@ -28,7 +28,7 @@ bool StackEmpty(SqStack S)
     }
 }
 
-bool Push(SqStack &S, char x)
+bool Push(SqStack &S, ElemType x)
 {
     if(S.top == MaxSize - 1)
         return false; //Õ»Âú
@@ -37,7 +37,7 @@ bool Push(SqStack &S, char x)
     return true;
 }
 
-bool Pop(SqStack &S, char &x)
+bool Pop(SqStack &S, ElemType &x)
 {
     if(S.top == -1)
         return false;
@@ -46,50 +46,5 @@ bool Pop(SqStack &S, char &x)
     return true;
 }
 
-bool brackerCheck(char str[], int length)
-{
-    int i;
-    SqStack S;
-    for(i = 0; i < length; i++)
-    {
-        if(str[i] == '(' || str[i] == '[' || str[i] == '{')
-            {
 
-                Push(S, str[i]);
-            }
-        else
-        {
-            if(StackEmpty(S))
-                return false;
-
-            char topElem;
-            Pop(S, topElem);
-
-            if(str[i] == ')' && topElem != '(')
-                return false;
-            if(str[i] == ']' && topElem != '[')
-                return false;
-            if(str[i] == '}' && topElem != '{')
-                return false;
-        }
-    }
-    return StackEmpty(S);
-}
-
-int main()
-{
-    char s[5];
-    scanf("%s", s);
-
-
-    bool flag;
-    flag = brackerCheck(s, 5);
-
-    if(flag)
-        printf("YES\n");
-    else
-        printf("NO\n");
-    return 0;
-
-}
 
