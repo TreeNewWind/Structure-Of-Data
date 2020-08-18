5d8fb0d58b71a3bc32adc2c1d479e5b2e5064e36
TreeNewWind <31409729+TreeNewWind@users.noreply.github.com>
2020-08-18 16:55:05 +0800

    循环单链表

---
+++ 循环链表/main.cpp
@@ -0,0 +1,41 @@
+#include<stdio,h>
+#include<stdlib.h>
+
+typedef struct LNode
+{
+
+    ElemType data;
+    struct LNode *next;
+}LNode, *LinkList;
+
+bool InitList(LinkList &L)
+{
+    L = (LNode *)malloc(sizeof(LNode));
+    if(L == NULL)
+        return false;
+    L->next = L;
+    return true;
+}
+
+bool Empty(LinkList L)
+{
+    if(L->next == L)
+        return true;
+    else
+        return false;
+}
+
+bool isTail(LinkList L, LNode *p)
+{
+    if(p->next == L)
+    {
+        return true;
+    }
+    else
+        return false;
+}
+
+
+
+
+
